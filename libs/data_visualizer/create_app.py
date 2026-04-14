e01932f80043b8e32d3e775765704384d149fb23
Jomi ade <>
2026-04-13 23:35:49 -0400

    updated some project files

--- libs/data_visualizer/create_app.py
+++ libs/data_visualizer/create_app.py
@@ -1,13 +1,15 @@
 from quart import Quart
-from config_loader import load_config
+# from config_loader import load_config
 from routes.pages import pages_bp
 from routes.partials import partials_bp
 
+
+# add a config loader later
 def create_app():
     app = Quart(__name__)
-    config = load_config("data_visualizer")
-    app.config["DB_API"] = f"http://{config['dbhost']}:{config['dbport']}"
-    app.config["CAM_URL"] = config.get("camera_base_url", "http://localhost:5001")
+    # config = load_config("data_visualizer")
+    # app.config["DB_API"] = f"http://{config['dbhost']}:{config['dbport']}"
+    # app.config["CAM_URL"] = config.get("camera_base_url", "http://localhost:5001")
     app.register_blueprint(pages_bp)
     app.register_blueprint(partials_bp)
     return app
