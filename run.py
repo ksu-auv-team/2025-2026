e0d028e9cf89182b0f20aac282130b44911ee2e1
msle237.lees <msle237.lees@gmail.com>
2026-04-22 19:39:09 -0400

    hwi test

--- run.py
+++ run.py
@@ -8,29 +8,36 @@ def main():
 
     db_manager_run = os.path.join(current_dir, 'libs/db_manager/run.sh')
     data_visualizer_run = os.path.join(current_dir, 'libs/data_visualizer/run.sh')
+    hardware_interface = os.path.join(current_dir, 'libs/hardware_interface/hardware_interface.py')
 
     logs_dir = os.path.join(current_dir, 'libs/data_visualizer/logs')
     os.makedirs(logs_dir, exist_ok=True)
 
     db_log = open(os.path.join(logs_dir, 'db_manager.log'), 'w')
     dv_log = open(os.path.join(logs_dir, 'data_visualizer.log'), 'w')
+    hw_log = open(os.path.join(logs_dir, 'hardware_interface.log'), 'w')
 
     db_proc = subprocess.Popen(['bash', db_manager_run], stdout=db_log, stderr=subprocess.STDOUT)
     dv_proc = subprocess.Popen(['bash', data_visualizer_run], stdout=dv_log, stderr=subprocess.STDOUT)
+    hw_proc = subprocess.Popen([sys.executable, hardware_interface], stdout=hw_log, stderr=subprocess.STDOUT)
 
     try:
         db_proc.wait()
         dv_proc.wait()
+        hw_proc.wait()
     except KeyboardInterrupt:
         print("Terminating processes...")
         db_proc.terminate()
         dv_proc.terminate()
+        hw_proc.terminate()
         db_proc.wait()
         dv_proc.wait()
+        hw_proc.wait()
         print("Processes terminated.")
     finally:
         db_log.close()
         dv_log.close()
+        hw_log.close()
         print("Logs saved in 'libs/data_visualizer/logs' directory.")
         sys.exit(0)
 
