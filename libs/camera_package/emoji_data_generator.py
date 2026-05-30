f1cea0acff20587b929b752044720a01a76bad24
Michael Lees <msle237.lees@gmail.com>
2026-05-30 16:21:00 -0400

    trying

--- libs/camera_package/emoji_data_generator.py
+++ libs/camera_package/emoji_data_generator.py
@@ -32,7 +32,7 @@ import numpy as np
 # ─────────────────────────────────────────────
 CONFIG = {
     # Paths
-    "pdf_path":         "/Users/michael/Documents/Projects/2025-2026/Robosub_Emojies.pdf",
+    "pdf_path":         "/Users/user/2025-2026/Robosub_Emojies.pdf",
     "emoji_dir":        "model_training/emojis/emoji_assets",
     "bg_dir":           "model_training/emojis/backgrounds",
     "dataset_dir":      "model_training/emojis/dataset",
